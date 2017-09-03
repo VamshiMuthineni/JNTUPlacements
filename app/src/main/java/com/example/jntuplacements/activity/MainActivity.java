8a8291dbae0b39df32c4e677e13e6155d43d6535
Muthineni Vamshi <vamshi.m@zohocorp.com>
2017-09-04 02:51:20 +0530

    Basic navigation drawer and list view on selecting items

---
+++ app/src/main/java/com/example/jntuplacements/activity/MainActivity.java
@@ -0,0 +1,186 @@
+package com.example.jntuplacements.activity;
+
+import android.app.ListFragment;
+import android.content.Intent;
+import android.content.res.Configuration;
+import android.media.Image;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
+import android.view.View;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.jntuplacements.R;
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
+import com.example.jntuplacements.fragment.HomeFragment;
+import com.example.jntuplacements.fragment.VisitedFragment;
+import com.example.jntuplacements.fragment.VisitingFragment;
+import com.example.jntuplacements.other.CircleTransform;
+
+import java.net.URI;
+
+public class MainActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener,VisitingFragment.OnFragmentInteractionListener, VisitedFragment.OnFragmentInteractionListener{
+
+    public static int navItemIndex = 0;
+
+    private static final String TAG_HOME = "home";
+    private static final String TAG_VISITING = "visiting";
+    private static final String TAG_VISITED = "visited";
+    private static String CURRENT_TAG = TAG_HOME;
+
+    private DrawerLayout drawer;
+    private NavigationView navigationView;
+    private View navHeader;
+    private FloatingActionButton fab;
+    private Handler mHandler;
+    private TextView txtName, txtWebsite;
+    private Toolbar toolbar;
+    private ImageView imgNavHeaderBg, imgProfile;
+    private String activityTitles[];
+    private ActionBarDrawerToggle actionBarDrawerToggle;
+
+    private static final String urlProfileImg = "https://lh3.googleusercontent.com/eCtE_G34M9ygdkmOpYvCag1vBARCmZwnVS6rS5t4JLzJ6QgQSBquM0nuTsCpLhYbKljoyS-txg";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+
+        navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+        navHeader = navigationView.getHeaderView(0);
+        txtName = (TextView) navHeader.findViewById(R.id.name);
+        txtWebsite = (TextView) navHeader.findViewById(R.id.website);
+
+        activityTitles = getResources().getStringArray(R.array.nav_item_activity_titles);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        actionBarDrawerToggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.syncState();
+
+        //load nav Header
+        txtName.setText("Vamshi Muthineni"); //logged in user data
+        txtWebsite.setText("website or email");
+
+        displaySelectedItem();
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        navItemIndex = item.getItemId();
+        displaySelectedItem();
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    private void displaySelectedItem(){
+        Fragment fragment = null;
+        switch (navItemIndex){
+            case R.id.nav_home:
+                navItemIndex = 0;
+                fragment = new HomeFragment();
+                break;
+            case R.id.nav_visiting:
+                navItemIndex = 1;
+                fragment = new VisitingFragment();
+                break;
+            case R.id.nav_visited:
+                navItemIndex = 2;
+                fragment = new VisitedFragment();
+                break;
+            default:
+                navItemIndex = 0;
+                fragment = new HomeFragment();
+        }
+
+        getSupportActionBar().setTitle(activityTitles[navItemIndex]);
+        navigationView.getMenu().getItem(navItemIndex).setChecked(true);
+
+        if(fragment!=null){
+            FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+            ft.replace(R.id.frame, fragment);
+            ft.commit();
+        }
+    }
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState){
+        super.onPostCreate(savedInstanceState);
+        actionBarDrawerToggle.syncState();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        actionBarDrawerToggle.onConfigurationChanged(newConfig);
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onVisitedFragmentInteraction(Uri uri) {
+        // Do different stuff
+    }
+
+    @Override
+    public void onVisitingFragmentInteraction(Uri uri) {
+
+    }
+}
---
+++ app/src/main/java/com/example/jntuplacements/fragment/HomeFragment.java
@@ -0,0 +1,64 @@
+package com.example.jntuplacements.fragment;
+
+import android.app.ListFragment;
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.jntuplacements.R;
+
+import org.w3c.dom.Text;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Activities that contain this fragment must implement the
+ * {@link HomeFragment.OnFragmentInteractionListener} interface
+ * to handle interaction events.
+ * Use the {@link HomeFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class HomeFragment extends Fragment {
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+    private View view = null;
+    public HomeFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.fragment_home, container, false);
+        TextView visitedList = (TextView) view.findViewById(R.id.home_text);
+        visitedList.setText("Welcome to JNTU Placement Portal");
+        return view;
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+    }
+}
